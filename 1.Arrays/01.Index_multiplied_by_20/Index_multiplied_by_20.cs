0e1eb83ea63f99db222dc07819221e4087bba513
bonethugs41 <pavlov.martin@gmail.com>
2013-12-19 16:20:49 +0200

    1.Arrays homework

---
+++ 1.Arrays/01.Index_multiplied_by_20/Index_multiplied_by_20.cs
@@ -0,0 +1,19 @@
+﻿//Write a program that allocates array of 20 integers and initializes each element by its index multiplied by 5.
+//Print the obtained array on the console.
+
+
+using System;
+
+class MultipliedIndexes
+{
+    static void Main()
+    {
+        Console.Title = "Indexes multiplied by 20";
+        int[] array = new int[20];
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = i * 5;
+            Console.WriteLine(array[i]);
+        }
+    }
+}
