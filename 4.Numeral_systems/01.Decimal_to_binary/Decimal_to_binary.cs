426f8a21a67f9c081d80cdac4c67d3d98a8d5559
Martin Pavlov <pavlov.martin@gmail.com>
2014-01-07 01:12:51 +0200

    4.Numeral systems
    1, 2

---
+++ 4.Numeral_systems/01.Decimal_to_binary/Decimal_to_binary.cs
@@ -0,0 +1,87 @@
+﻿//Write a program to convert decimal numbers to their binary representation.
+
+
+using System;
+
+class DecimalBinary
+{
+    static bool isNegative = false;
+    static int IntegerCheck(string check)                                       //Check if the input information is integer
+    {
+        while (true)
+        {
+            int integer;
+            bool result = int.TryParse(check, out integer);
+            if (result == true)
+            {
+                return integer;
+            }
+            else
+            {
+                Console.Write("Invalid integer, try again: ");
+                check = Console.ReadLine();
+            }
+        }
+    }
+
+    static void Converter(int number)
+    {
+        int remainder;
+        int[] array = new int[32];
+        for (int i = 0; i < array.Length; i++)
+        {
+            remainder = number % 2;
+            number = number / 2;
+            array[i] = remainder;
+        }
+        ReverseArray(array);
+    }
+   
+
+    static void ReverseArray(int[] array)                                       
+    {
+        int temp;
+        if (isNegative == false)
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                temp = array[i];
+                array[i] = array[array.Length - i - 1];
+                array[array.Length - i - 1] = temp;
+            }
+            array[0] = 1;
+        }
+        Printig(array);
+    }
+
+    static void Printig(int[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write(array[i]);
+        }
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        Console.Title = "Decimal to binary converter";
+        Console.Write("Input some number: ");
+        int number = IntegerCheck(Console.ReadLine());
+        if (number < 0)
+        {
+            number = Int32.MaxValue + number + 1;
+            isNegative = true;
+        }
+        Converter(number);
+    }
+}
---
+++ 4.Numeral_systems/02.Binary_to_decimal/Binary_to_decimal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class BinaryDecimal
+{
+    static int IntegerCheck(string check)                                       //Check if the input information is integer
+    {
+        while (true)
+        {
+            int integer;
+            bool result = int.TryParse(check, out integer);
+            if (result == true)
+            {
+                return integer;
+            }
+            else
+            {
+                Console.Write("Invalid integer, try again: ");
+                check = Console.ReadLine();
+            }
+        }
+    }
+
+    static int Converter(string biNumber)
+    {
+        int sum = 0;
+        int index = 0;
+        int power = 1;
+        for (int i = biNumber.Length - 1; i >= 0; i--)
+        {
+            if (biNumber[i] == '0')
+            {
+                index++;
+                continue;
+            }
+            else if (biNumber[i] == '1')
+            {
+                for (int j = 0; j < index; j++)                             
+                {
+                    power = power * 2;
+                }
+                //power = (int)Math.Pow(2, index);
+                sum = sum + power;
+                index++;
+                power = 1;
+            }
+
+        }
+        return sum;
+    }
+
+    static void Printing(int result)
+    {
+        Console.WriteLine("The number is: {0}", result);
+    }
+
+    static void Main()
+    {
+        Console.Title = "Binary to decimal convertor";
+        Console.Write("Enter a binary number: ");
+        int number = IntegerCheck(Console.ReadLine());
+        string biNumber = number.ToString();
+        int result = Converter(biNumber);
+        Printing(result);
+    }
+}
