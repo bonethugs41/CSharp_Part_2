eff8578a83e41acb390e71075bf768bfa5ab5700
Martin Pavlov <pavlov.martin@gmail.com>
2014-01-15 22:52:03 +0200

    8.Strings and text processing
    v1

---
+++ 8.Strings_and_text_processing/02.Reverese_string/Program.cs
@@ -0,0 +1,24 @@
+﻿//Write a program that reads a string, reverses it and prints the result at the console.
+//Example: "sample" => "elpmas".
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Title = "Reverse a string";
+        Console.Write("Write some text: ");
+        string s = Console.ReadLine();
+        string reverseString = ReverseString(s);
+        Console.WriteLine(reverseString);
+    }
+
+    private static string ReverseString(string s)
+    {
+        char[] charArray = s.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+}
