3a0a8f1b076e512f1f5f88d1e1a93da600e92498
Martin Pavlov <pavlov.martin@gmail.com>
2014-01-20 01:36:06 +0200

    Genome decoder

---
+++ 9.Exam_preparation/06.Genome_decoder/Genome_decoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Genome_decoder
+{
+    class GenomeDecoder
+    {
+
+        static string GetFromString(string digit)
+        {
+            string result = "NO";
+            switch (digit)
+            {
+                case "A": ; break;
+                case "G": ; break;
+                case "T": ; break;
+                case "C": ; break;
+                default:
+                    break;
+            }
+            return result;
+        }
+
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] numbers = input.Split(' ');
+            int n = int.Parse(numbers[0]);
+            int m = int.Parse(numbers[1]);
+            string inputCode = Console.ReadLine();
+
+        }
+    }
+}
