ae281164199d97430c92f3ff19c717ae1e891c0b
Martin Pavlov <pavlov.martin@gmail.com>
2014-01-20 00:07:51 +0200

    9. Exam preparation
    9gag numbers

--- 9.Exam_preparation/04.Durankulak_numbers/Durankulak_numbers.cs
+++ 9.Exam_preparation/04.Durankulak_numbers/Durankulak_numbers.cs
@@ -10,8 +10,67 @@ namespace _04.Durankulak_numbers
     {
         static void Main()
         {
-            string durankulakNumber = Console.ReadLine();
+            string input = Console.ReadLine();
+            int index = 0;
+            long sum = 0L;
+            int digits = -1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] >= 97)
+                {
+                    digits++;
+                    i++;
+                }
+                else
+                {
+                    digits++;
+                }
+            }
+            string[] letters = new string[168];
+            int move = 0;
 
+            for (int i = 0; i < 26; i++)
+            {
+                letters[i] = ((char)('A' + move)).ToString();
+                move++;
+            }
+            int position = 26;
+
+            for (char i = 'a'; i <= 'z'; i++)
+            {
+                for (char j = 'A'; j <= 'Z'; j++)
+                {
+                    if (position == 168)
+                    {
+                        break;
+                    }
+                    letters[position] = i.ToString() + j.ToString();
+                    position++;
+                }
+            }
+            string search;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                search = input[i].ToString();
+                if (input[i] >= 97)
+                {
+                    search = input[i].ToString() + input[i + 1];
+                    i++;
+                }
+
+                for (int j = 0; j < letters.Length; j++)
+                {
+                    if (search == letters[j])
+                    {
+                        index = j;
+                        sum = sum + index * (long)(Math.Pow(168, digits));
+                        break;
+                    }
+                }
+                digits--;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
---
+++ 9.Exam_preparation/05.9GAG_numbers/GAG_numbers.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace _05._9GAG_numbers
+{
+    class GagNumbers
+    {
+        static string ConvertGagString(string digit)
+        {
+            string result = "NO";
+            switch (digit)
+            {
+                case "-!": return "0"; break;
+                case "**": return "1"; break;
+                case "!!!": return "2"; break;
+                case "&&": return "3"; break;
+                case "&-": return "4"; break;
+                case "!-": return "5"; break;
+                case "*!!!": return "6"; break;
+                case "&*!": return "7"; break;
+                case "!!**!-": return "8"; break;
+
+                default:
+                    break;
+            }
+            return result;
+        }
+
+        //static BigInteger HexToDec(string hexNumber)
+        //{
+        //    long power = 1;
+        //    long index = 0;
+        //    BigInteger sum = 0;
+        //    for (int i = hexNumber.Length - 1; i >= 0; i--)
+        //    {
+        //        switch (hexNumber[i])
+        //        {
+        //            case '0':
+        //                index++;
+        //                break;
+        //            case '1':
+        //                power = 1 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '2':
+        //                power = 2 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '3':
+        //                power = 3 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '4':
+        //                power = 4 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '5':
+        //                power = 5 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '6':
+        //                power = 6 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '7':
+        //                power = 7 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '8':
+        //                power = 8 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //            case '9':
+        //                power = 9 * (long)Math.Pow(9, index);
+        //                sum = sum + power;
+        //                index++;
+        //                break;
+        //        }
+        //    }
+        //    return sum;
+        //}
+
+        static BigInteger NinePower(int number)
+        {
+            BigInteger result = 1;
+            for (int i = 0; i < number; i++)
+            {
+                result = result * 9;
+            }
+            return result;
+        }
+
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            string partialInput = string.Empty;
+            string nineSystemNumber = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                partialInput = partialInput + input[i];
+                string currentDigit = ConvertGagString(partialInput);
+
+                if (currentDigit != "NO")
+                {
+                    nineSystemNumber += currentDigit;
+                    partialInput = "";
+                }
+            }
+
+            BigInteger result = 0;
+
+            for (int i = 0; i < nineSystemNumber.Length; i++)
+            {
+                BigInteger digit = int.Parse(nineSystemNumber[i].ToString());
+                //result = result + digit * (int)Math.Pow(9, nineSystemNumber.Length  - 1 - i);
+
+                result = result + digit * NinePower(nineSystemNumber.Length - 1 - i);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
