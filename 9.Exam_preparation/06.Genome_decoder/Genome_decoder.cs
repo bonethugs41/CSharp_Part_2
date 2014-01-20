47b37eb13a3e4a234594945611c97d4c0f56f8b7
Martin Pavlov <pavlov.martin@gmail.com>
2014-01-21 01:34:15 +0200

    Genome decoder

--- 9.Exam_preparation/06.Genome_decoder/Genome_decoder.cs
+++ 9.Exam_preparation/06.Genome_decoder/Genome_decoder.cs
@@ -9,21 +9,6 @@ namespace _06.Genome_decoder
     class GenomeDecoder
     {
 
-        static string GetFromString(string digit)
-        {
-            string result = "NO";
-            switch (digit)
-            {
-                case "A": ; break;
-                case "G": ; break;
-                case "T": ; break;
-                case "C": ; break;
-                default:
-                    break;
-            }
-            return result;
-        }
-
 
         static void Main()
         {
@@ -33,6 +18,68 @@ namespace _06.Genome_decoder
             int m = int.Parse(numbers[1]);
             string inputCode = Console.ReadLine();
 
+            StringBuilder decodedGenome = DecodeGenome(inputCode);
+            //  Console.WriteLine(decodedGenome);
+
+            PrintOutput(decodedGenome, m, n);
+        }
+
+        private static void PrintOutput(StringBuilder decodedGenome, int spaces, int lines)
+        {
+            string printing = decodedGenome.ToString();
+            int counter = 1;
+            int counterLines = 1;
+            Console.Write(1 + " ");
+
+            for (int i = 0; i < printing.Length; i++)
+            {
+                Console.Write(printing[i]);
+                if (i % spaces == 0)
+                {
+                    Console.Write(" ");
+                    counter++;
+                }
+                if (i == printing.Length - 1)
+                {
+                    Console.WriteLine();
+                    break;
+                }
+                if (i + 1 == lines * counterLines)
+                {
+                    Console.WriteLine();
+                    Console.Write(counter + 1 + " ");
+                    counterLines++;
+                }
+
+
+            }
+        }
+
+        private static StringBuilder DecodeGenome(string inputCode)
+        {
+            StringBuilder decodedGenom = new StringBuilder();
+            StringBuilder repeatTimesString = new StringBuilder();
+
+            foreach (char item in inputCode)
+            {
+                if (item == 'A' || item == 'G' || item == 'C' || item == 'T')
+                {
+                    int repeatTimes = 1;
+                    if (repeatTimesString.Length != 0)
+                    {
+                        repeatTimes = int.Parse(repeatTimesString.ToString());
+                        repeatTimesString.Clear();
+                    }
+                    string genomeSubseq = new string(item, repeatTimes);
+                    decodedGenom.Append(genomeSubseq);
+                }
+                else
+                {
+                    repeatTimesString.Append(item);
+                }
+            }
+
+            return decodedGenom;
         }
     }
 }
