8afa817dc279e285b6d884eb8f6dd6dddbcb2545
Martin Pavlov <pavlov.martin@gmail.com>
2014-01-22 00:42:22 +0200

    9. Exam preparation
    Magic words

--- 9.Exam_preparation/06.Genome_decoder/Genome_decoder.cs
+++ 9.Exam_preparation/06.Genome_decoder/Genome_decoder.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+//Don't work correctly
 namespace _06.Genome_decoder
 {
     class GenomeDecoder
---
+++ 9.Exam_preparation/07.Magic_words/MagicWords.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.Magic_words
+{
+    class MagicWords
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var words = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                words.Add(Console.ReadLine());
+            }
+
+            //Reorder
+
+            for (int i = 0; i < n; i++)
+            {
+                var word = words[i];
+                var newIndex = word.Length % (n + 1);
+
+                //words[i] = null;
+                //words.Insert(newIndex, word);
+                //words.Remove(null);
+
+                words.Insert(newIndex, word);
+                if (newIndex < i)
+                {
+                    words.RemoveAt(i + 1);
+                }
+                else if (newIndex >= i)
+                {
+                    words.RemoveAt(i);
+                }
+            }
+
+            //print
+
+            var maxLength = 0;
+            foreach (var word in words)
+            {
+                maxLength = Math.Max(maxLength, word.Length);                               //Gives the longest word
+            }
+
+            //var maxLength = words.Max(x => x.Length);
+
+
+            var result = new StringBuilder();
+            for (int i = 0; i < maxLength; i++)
+            {
+                foreach (var word in words)
+                {
+                    if (word.Length > i)
+                    {
+                        result.Append(word[i]);
+                    }
+                }
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
